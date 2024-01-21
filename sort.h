3d41569058385d8896a14a4f6217bc0e11915a27
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-21 03:38:47 +0200

    FixCodeStyle

--- sort.h
+++ sort.h
@@ -11,9 +11,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
