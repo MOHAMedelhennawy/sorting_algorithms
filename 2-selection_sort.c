1cbb28f8d9f93c390e82bc446eeaa56195e87f1c
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-21 03:40:55 +0200

    AddSelectionSort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,25 +1,29 @@
 #include "sort.h"
-
+/**
+ * selection_sort - Sorting function.
+ * @array: Array to srot.
+ * @size: Size of array.
+*/
 void selection_sort(int *array, size_t size)
 {
-    int min, temp;
-    int idx = 0;
-    size_t i, j;
+	int min, temp;
+	int idx = 0;
+	size_t i, j;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min = array[i];
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < min)
-            {
-                min = array[j];
-                idx = j;
-            }
-        }
-        temp = array[i];
-        array[i] = min;
-        array[idx] = temp;
-        print_array(array, size);
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < min)
+			{
+				min = array[j];
+				idx = j;
+			}
+		}
+		temp = array[i];
+		array[i] = min;
+		array[idx] = temp;
+		print_array(array, size);
+	}
 }
