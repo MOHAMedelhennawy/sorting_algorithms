a9739b61e729e9bf9c4b89cca5c717f9e058395a
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-01-22 23:09:15 +0200

    FixCode

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,7 +8,7 @@
 void counting_sort(int *array, size_t size)
 {
 	size_t max = array[0], i;
-	int *count;
+	int *count_arr;
 	int *sorted_array;
 
 	if (!array || size < 2)
@@ -20,31 +20,32 @@ void counting_sort(int *array, size_t size)
 			max = array[i];
 	}
 
-	count = (int *)malloc(sizeof(int) * max);
-	if (!count)
+	count_arr = (int *)malloc(sizeof(int) * max + 1);
+	if (!count_arr)
 		return;
 
-	for (i = 0; i <= max; i++)
-		count[i] = 0;
+	for (i = 0; i < max; i++)
+		count_arr[i] = 0;
 
 	for (i = 0; i < size; i++)
-		count[array[i]]++;
+		count_arr[array[i]]++;
 
 	for (i = 1; i <= max; i++)
-		count[i] += count[i - 1];
+		count_arr[i] += count_arr[i - 1];
 
 	sorted_array = (int *) malloc(sizeof(int) * size);
 	if (!sorted_array)
 		return;
-	print_array(count, max + 1);
+
+	print_array(count_arr, max + 1);
 	for (i = size - 1; (int)i >= 0; i--)
 	{
-		count[array[i]]--;
-		sorted_array[count[array[i]]] = array[i];
+		count_arr[array[i]]--;
+		sorted_array[count_arr[array[i]]] = array[i];
 	}
 
 	for (i = 0; i < size; i++)
 		array[i] = sorted_array[i];
 
-	free(count), free(sorted_array);
+	free(count_arr), free(sorted_array);
 }
