b7909e9b2a90634248c59bf3d0a852ec96b661f8
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2025-02-13 09:11:06 +0200

    Add Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,17 +11,16 @@ void selection_sort(int *array, size_t size)
 	if (!array || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 				min = j;
 		}
 
-		if (min != j)
+		if (min != i)
 		{
 			swap(&array[min], &array[i]);
 			print_array(array, size);
